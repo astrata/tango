be4af0b623f37fb3b47e4503dcaacb89a0115ce6
Jose Carlos Nieto <xiam@menteslibres.org>
2012-10-18 17:12:22 -0500

    Adding LICENSE and documentation to some files.

--- tango-base-app/src/models/static.go
+++ tango-base-app/src/models/static.go
@@ -1,3 +1,29 @@
+/*
+  Tango!
+
+  Copyright (c) 2012 Astrata Software, <http://astrata.mx>
+  Written by José Carlos Nieto <xiam@menteslibres.org>
+
+  Permission is hereby granted, free of charge, to any person obtaining
+  a copy of this software and associated documentation files (the
+  "Software"), to deal in the Software without restriction, including
+  without limitation the rights to use, copy, modify, merge, publish,
+  distribute, sublicense, and/or sell copies of the Software, and to
+  permit persons to whom the Software is furnished to do so, subject to
+  the following conditions:
+
+  The above copyright notice and this permission notice shall be
+  included in all copies or substantial portions of the Software.
+
+  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+  EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+  NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE
+  LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+  OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION
+  WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 package models
 
 import (
@@ -9,19 +35,23 @@ import (
 	"strings"
 )
 
-// Root directory for static files.
+// Root directory for serving static files.
 var Root = "static"
 
+// Model name
 type Static struct {
 }
 
+// Initialization function
 func init() {
+	// Your initialization code goes here.
 	app.Register("Static", &Static{})
 	app.Fallback("/", app.App("Static"))
 }
 
-// Checking root directory on start.
+// Model's StartUp() function
 func (self *Static) StartUp() {
+	// Code to be executed when all the models are loaded and fully initialized.
 	info, err := os.Stat(Root)
 	if err == nil {
 		if info.IsDir() == false {
